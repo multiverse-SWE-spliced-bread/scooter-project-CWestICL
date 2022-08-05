97cf4f9fd4a871b8d192713ef2c263d9a2dff677
CWestICL <cwest@ic.ac.uk>
2022-08-05 12:18:45 +0100

    Init commit

--- src/User.js
+++ src/User.js
@@ -1,3 +1,14 @@
+class Person {
+  constructor(name,age,email) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+  register() {
+    
+  }
+}
+
 class User {
     // User code here
   }
